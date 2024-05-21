f1cf9bcd43a70968bd88473f3424263af137cbd3
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2024-05-21 19:32:14 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp01.Consola/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace ConsoleApp01.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumaNros = 0;
+            int contadorPares = 0;
+            int contadorMayores10 = 0;
+            Console.Write("Ingrese la cantidad de números:");
+            if (!int.TryParse(Console.ReadLine(), out int cantidadNumeros))
+            {
+                Console.WriteLine("Cantidad mal ingresada");
+                return;
+            }
+            if (cantidadNumeros>0)
+            {
+                
+                for (int contador = 1; contador <= cantidadNumeros; contador++)
+                {
+                    Console.Write($"Ingrese el {contador} número:");
+                    int numeroIngresado = int.Parse(Console.ReadLine());
+                    sumaNros+= numeroIngresado;//sumaNros=sumaNros+numeroIngresado
+                    if (EsPar(numeroIngresado))
+                    {
+                        contadorPares++;
+                    }
+                    if (EsMayor10(numeroIngresado))
+                    {
+                        contadorMayores10++;
+                    }
+                    Console.WriteLine($"Suma Parcial: {sumaNros}");
+                }
+                var promedioNros = sumaNros / cantidadNumeros;
+                Console.WriteLine($"Suma de números: {sumaNros}");
+                Console.WriteLine($"Promedio.......: {promedioNros}");
+                Console.WriteLine($"Cant. de Pares.: {contadorPares}");
+                Console.WriteLine($"Cant. Mayores 10: {contadorMayores10}");
+            }
+            else
+            {
+                Console.WriteLine("La cantidad debe ser superior a 0");
+            }
+        }
+
+        private static bool EsMayor10(int numeroIngresado) => numeroIngresado > 10;
+        
+
+        private static bool EsPar(int numeroIngresado) => numeroIngresado % 2 == 0;
+        
+    }
+}
---
+++ ConsoleApp06.Consola/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace ConsoleApp06.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contadorNumeros = 0;
+            int contadorDePares = 0;
+            int mayorNumero = 0;
+            int menorNumero = 0;
+            int sumaNumeros = 0;
+            Console.Write("Ingrese la suma tope del ciclo:");
+            int tope=int.Parse(Console.ReadLine());
+            do
+            {
+                Console.Write("Ingrese un numero:");
+                int numeroIngresado=int.Parse(Console.ReadLine());
+                contadorNumeros++;
+                sumaNumeros += numeroIngresado;
+                if (EsPar(numeroIngresado))
+                {
+                    contadorDePares++;
+                }
+                if (contadorNumeros==1)
+                {
+                    mayorNumero = numeroIngresado;
+                    menorNumero=numeroIngresado;
+                }
+                else
+                {
+                    if (numeroIngresado>mayorNumero)
+                    {
+                        mayorNumero = numeroIngresado;
+                    }
+                    if (numeroIngresado<menorNumero)
+                    {
+                        menorNumero = numeroIngresado;
+                    }
+                }
+
+            } while (sumaNumeros<=tope);
+            Console.WriteLine($"Suma de números: {sumaNumeros}");
+            Console.WriteLine($"Cantidad Números:{contadorNumeros}");
+            Console.WriteLine($"Cantidad de Pares:{contadorDePares}");
+            Console.WriteLine($"Mayor número ingresado: {mayorNumero}");
+            Console.WriteLine($"Menor número ingresado: {menorNumero}");
+        }
+
+        private static bool EsPar(int numeroIngresado)
+            => numeroIngresado % 2 == 0;
+        
+    }
+}
